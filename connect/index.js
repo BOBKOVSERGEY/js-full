dd5a8026192d05e0c5f162ede55915d09d743670
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-01-17 16:18:34 +0300

    Типы данных

--- connect/index.js
+++ connect/index.js
@@ -3,3 +3,12 @@
 // переменные let создаются в момент использования
 // let видна только в блоке ограниченном фигурными скобками
 let second = 2;
+
+// типы данных 
+var number = 5; // число 
+var string = 'Hello'; // строка 
+var sym = Symbol(); // символ
+var boolean = true; // логическое значение
+null; // null
+undefined; // undefined
+var obj = {}; // объект
