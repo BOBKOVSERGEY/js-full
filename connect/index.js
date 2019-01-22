2d6dd618652dcf45c41bff2f8800676a1535cfbd
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-01-22 16:44:44 +0300

    Проверка на тип данных

--- connect/index.js
+++ connect/index.js
@@ -9,6 +9,18 @@ var number = 5; // число
 var string = 'Hello'; // строка 
 var sym = Symbol(); // символ
 var boolean = true; // логическое значение
-null; // null
+null; // null - это ссылка на не существующий объект 
 undefined; // undefined
-var obj = {}; // объект
+var obj = {}; // объект - коллекция данных
+
+// проверка на тип данных
+console.log(typeof(obj));
+
+
+let person = {
+  name: 'John',
+  age: 25,
+  isMarried: false
+};
+
+console.log(`${person.name} hello`);
