428b8811b251619aa40e1ca6081b62e67acc4c23
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-01-28 17:20:04 +0300

    Switch

--- conditions/index.js
+++ conditions/index.js
@@ -5,3 +5,21 @@ let num = 50;
 //если участвует одно условие то унарный инкремент или  декремент
 //если участвует два условия то бинарный
 //если участвует три условия то тернарный 
+
+switch(num) {
+  case num < 49:
+    console.log('Неверно');
+    break;
+  case num > 100:
+    console.log('Много');
+    break;
+  case num > 80:
+    console.log('Все еще много');
+    break;
+  case 50:
+    console.log('Верно');
+    break;
+  default:
+    console.log('Something wrong');
+    break;
+}
