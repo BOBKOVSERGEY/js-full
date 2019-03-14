5de67321a21475a92dbdb257e97447e37e4a995c
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-03-14 16:50:59 +0300

    Start budgetApp

--- sobytiya-obrabotchiki/index.js
+++ sobytiya-obrabotchiki/index.js
@@ -1,11 +1,12 @@
-let btn = document.getElementsByTagName('button'),
-    wrap = document.querySelector('.wrapper');
+let btn = document.querySelectorAll('button'),
+    wrap = document.querySelector('.wrapper'),
+    link = document.querySelector('a');
 //console.log(btn);
 
 btn[0].addEventListener('click', function(event) {
   let target = event.target;
   //target.style.display = 'none';
-  //console.log(event);
+  console.log(event);
   console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
 });
 
@@ -21,3 +22,15 @@ btn[0].addEventListener('mouseleave', function(e) {
 wrap.addEventListener('click', function(event) {
   console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
 });
+
+link.addEventListener('click', function(event) {
+  event.preventDefault();
+  console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
+});
+
+// item - это каждая кнопка которая есть на странице
+btn.forEach(function(item, i) {
+  item.addEventListener('mouseleave', function() {
+    console.log('Мы вышли с кнопки ' + (i+1));
+  });
+}) ;
