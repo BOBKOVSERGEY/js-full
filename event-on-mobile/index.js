79187e2fbe6f595673009a88cf37c0c5bc61d9e3
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-04-26 15:02:56 +0300

    События на мобильных устройствах

--- event-on-mobile/index.js
+++ event-on-mobile/index.js
@@ -4,3 +4,30 @@
 // touchenter - возникает, когда наш палец заходит на какойто элемент
 // touchleave - срабатывает, когда наш палец покинул элемент
 // touchcancel - срабатывает, когда точка соприкосновения больше не регистрируется на поверхности
+window.addEventListener('DOMContentLoaded', function () {
+  let box = document.querySelector('.box');
+  let link = document.querySelector('.link');
+  box.addEventListener('touchstart', function (e) {
+    console.log('touchStart');
+    console.log(e.target);
+    console.log(e.touches);
+    console.log(e.touches[0].target);
+    console.log(e.changedTouches);
+    console.log(e.targetTouches);
+  });
+  box.addEventListener('touchmove', function (e) {
+    e.preventDefault();
+    console.log('touchmove');
+  });
+
+  box.addEventListener('touchend', function (e) {
+    e.preventDefault();
+    console.log('touchend');
+  });
+  link.addEventListener('touchstart', function (e) {
+    e.preventDefault();
+    console.log('touch link');
+
+  });
+});
+
